c3b61c3e314a3d6a1f902c23390141f9804f2267
Joshua Park <js4park@gmail.com>
2020-03-18 00:51:07 -0700

    working location

---
+++ NearMe/LocationSearchTable.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchTable.swift
+//  NearMe
+//
+//  Created by Joshua Park on 3/17/20.
+//  Copyright © 2020 NearMe. All rights reserved.
+//
+
+import UIKit
+class LocationSearchTable : UITableViewController {
+    
+}
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
--- NearMe/View Controllers/Home_ViewController.swift
+++ NearMe/View Controllers/Home_ViewController.swift
@@ -10,25 +10,74 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class Home_ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
+class Home_ViewController: UIViewController,MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var resultSearchController:UISearchController? = nil
+    
+   // var didFindLocation
 
     fileprivate let locationManager:CLLocationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        //resultSearchController?.dimsBackgroundDuringPresentation = true
+        resultSearchController?.obscuresBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.distanceFilter = kCLDistanceFilterNone
-        locationManager.startUpdatingLocation()
-
+        //locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestLocation()
+        var location: CLLocation!
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways) {
+            location = locationManager.location
+        }
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        if location != nil {
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     
 
 }
 
-
+extension Home_ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("hiiiiiiiiii")
+        if let location = locations.first {
+            print("hellooooo")
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta:0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("error \(error)")
+            print("hiiiialksdjfklasjdf")
+    }
+}
 
